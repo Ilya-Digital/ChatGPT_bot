8970e3775ed950f693c982c2c8545da4b203f3dc
IlyaIvanov <ilvat777@gmail.com>
2024-06-22 11:33:14 +0300

    first commit

---
+++ app/handlers.py
@@ -0,0 +1,86 @@
+from aiogram import Router, Bot
+from aiogram.filters import CommandStart, Filter, Command
+from aiogram.types import Message
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from openai import OpenAI
+import httpx
+
+from app.database.requests import set_user, get_users
+
+router = Router()
+
+ADMINS = ['id', 'id']
+
+client = OpenAI(
+    api_key='Ключ OpenAI',
+    http_client=httpx.Client(
+        proxies="Прокси",
+        transport=httpx.HTTPTransport(local_address="0.0.0.0"),
+    ),
+)
+
+
+def generate_answer(user_message):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": f"{user_message}",
+            }
+        ],
+        model="gpt-4",
+    )
+    return chat_completion.choices[0].message.content
+
+
+class AdminProtect(Filter):
+    def __init__(self):
+        self.admins = ADMINS
+    
+    async def __call__(self, message: Message):
+        return message.from_user.id in self.admins
+
+
+class AntiFlood(StatesGroup):
+    generating_message = State()
+
+
+class Newsletter(StatesGroup):
+    message = State()
+
+
+@router.message(CommandStart())
+async def cmd_start(message: Message, state: FSMContext):
+    await set_user(message.from_user.id)
+    await message.answer(text='Привет!')
+
+
+@router.message(AdminProtect(), Command('newsletter'))
+async def admin(message: Message, state: FSMContext):
+    await state.set_state(Newsletter.message)
+    await message.answer('Отправьте сообщение для рассылки!')
+
+
+@router.message(AdminProtect(), Newsletter.message)
+async def get_admin(message: Message, state: FSMContext, bot: Bot):
+    users = await get_users()
+    for user in users:
+        try:
+            await bot.send_message(chat_id=user.tg_id, text=message.text)
+        except:
+            print('user banned')
+    await message.answer('Рассылка завершена!')
+
+
+@router.message(AntiFlood.generating_message)
+async def anti_flood(message: Message, state: FSMContext):
+    await message.answer('Вы ещё не получили ответа! Если это ошибка, перезапустите бота /start.')
+
+
+@router.message()
+async def gpt_answer(message: Message, state: FSMContext):
+    await state.set_state(AntiFlood.generating_message)
+    answer = generate_answer(message.text)
+    await message.answer(f'{answer}')
+    await state.clear()
---
+++ run.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+
+from app.handlers import router
+from app.database.models import async_main
+
+
+async def main():
+    await async_main()
+    bot = Bot(token='Токен бота')
+    dp = Dispatcher()
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Error')
